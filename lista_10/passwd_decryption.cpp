5d2b7cb2b2689c4b8961fb6a9b0ef0e2bfb0ef15
Olaf Bieganski <obiegans@panamint.kcir.pwr.edu.pl>
2023-01-02 09:39:31 +0100

    zajecia nr 10

---
+++ lista_10/passwd_decryption.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <openssl/evp.h>
+#include <string.h>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+void bytes2md5(const char *data, int len, char *md5buf) {
+	EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+	const EVP_MD *md = EVP_md5();
+	unsigned char md_value[EVP_MAX_MD_SIZE];
+	unsigned int md_len, i;
+	EVP_DigestInit_ex(mdctx, md, NULL);
+	EVP_DigestUpdate(mdctx, data, len);
+	EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+	EVP_MD_CTX_free(mdctx);
+	for (i = 0; i < md_len; i++) {
+		snprintf(&(md5buf[i * 2]), 16 * 2, "%02x", md_value[i]);
+	}
+}
+
+int main(int argc, char* argv[]){
+    if (argc < 2)
+    {
+        perror("Argument 1 not specified. Filename required!\n");
+        exit(1);
+    }
+
+    vector<string> dictionary;
+    vector<string> hash;
+    char *filename = argv[1];
+	std::ifstream file(filename);
+	string line;
+    if( file.is_open() ) {
+		while ( file ) {
+			getline (file, line);;
+			dictionary.push_back(line);
+		}
+	}
+    char md5[33]; // 32 characters + null terminator
+    for(int i = 0; i < dictionary.size(); i++){
+      const char *word = dictionary[i].c_str();
+      bytes2md5(word, strlen(word), md5);
+      hash.push_back(md5);
+    }
+
+    
+
+    return 0;
+}
