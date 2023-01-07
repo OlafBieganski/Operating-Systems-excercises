f93535cb9082df73f8be3a95f3b1856ab2e87829
Olaf Bieganski <olaf.bieganski2001@gmail.com>
2023-01-07 16:10:33 +0100

    dodani pierwsi producenci

--- lista_10/passwd_decryption.cpp
+++ lista_10/passwd_decryption.cpp
@@ -4,9 +4,17 @@
 #include <vector>
 #include <fstream>
 #include <csignal>
+#include <array>
+#include <thread>
+#include <mutex>
 
-std::vector<std::string> dictionary;
-std::vector<std::string> hash;
+std::vector<std::string> dictionary, hash, emails;
+std::vector<std::array<std::string, 3>> passwdPairs; // first is pswd second hash, third email
+bool passwdFound = false; // flags  for threads
+bool continueFlag = false;
+std::array<std::thread, 6> threadArr;
+std::mutex passwdFound_mtx, passwdPairs_mtx; 
+
 
 // function to peform conversion from char array to hash code array
 void bytes2md5(const char *data, int len, char *md5buf) {
@@ -23,51 +31,135 @@ void bytes2md5(const char *data, int len, char *md5buf) {
 	}
 }
 
+// signal handling for SIGHUP
+void signalHandler( int sigcode ) {
+	std::cout << "Interrupt signal (" << sigcode << ") received.\n";
+	// signal reaction code
+	for(const auto &arr : passwdPairs)
+		std::cout << "The deciphered password is: " << arr[0]
+		 << ". Associated email: " << arr[2] << std::endl;  
+}
+
+enum class producerNR {zero, one, two};
+
+void producer1word(const std::string &_crrhash, producerNR _producer){
+	uint i = 0;
+	char md5[33]; // 32 characters + null terminator
+	while(continueFlag && !passwdFound){
+		std::string currWord = dictionary[i];
+		switch(_producer){
+			case producerNR::one:
+				currWord[0] = toupper(currWord[0]);
+				break;
+			case producerNR::two:
+				for(auto &letter : currWord)
+					letter = toupper(letter);
+				break;
+		}
+		const char *word = currWord.c_str();
+		bytes2md5(word, strlen(word), md5);
+		if(md5 == _crrhash){
+			// protect with mutex
+			passwdFound_mtx.lock();
+			passwdFound = true;
+			passwdFound_mtx.unlock();
+			std::array<std::string, 3> newPswd = {currWord, _crrhash, emails.back()};
+			passwdPairs_mtx.lock();
+			passwdPairs.push_back(newPswd);
+			passwdPairs_mtx.unlock();
+			return;
+		}
+		i++;
+	}
+}
+
+void producer1(const std::string &_crrhash){
+
+}
+
+template<std::size_t SIZE>
+void stopThreads(std::array<std::thread, SIZE> &thArr){
+	// stop all threads
+	continueFlag = false;
+	// joining threads to terminate them
+	for(int i = 0; i < SIZE; i++)
+		if(threadArr[i].joinable())
+			threadArr[i].join();
+}
+
 int main(int argc, char* argv[]){
+	// enable signal handling
+	signal(SIGHUP, signalHandler);
+
 	std::string userInput;
-	std::ifstream file;
+	std::ifstream passwdFile;
 	char *filename;
 
+	// Check if dictionary file is given
+    if (argc < 2)
+    {
+        std::cout << "Argument 1 not specified. Filename required!\n";
+        exit(-1);
+    }
+	// read dictionary to memory
+	std::string dictName = argv[1];
+	std::ifstream dictFile(dictName, std::ios::out);
+	if(!dictFile){
+		std::cout << "Invalid dictionary file argument." << std::endl;
+		exit(-1);
+	}
+	if(dictFile.is_open()){
+		std::string line;
+		while(dictFile){
+			std::getline(dictFile, line);
+			dictionary.push_back(line);
+		}
+		std::cout << "Dictionary " << dictName << " successfuly read to memory."
+		 << std::endl;
+	}
+
 	while(true){
-		std::getline(std::cin, userInput);
-		if(userInput == "q") break;
-		file.open(userInput, std::ios::out);
-		if(!file){
+		// get whole line from input
+		while(!std::getline(std::cin, userInput) || passwdFound);
+		// stop if q is input
+		if(userInput == "q"){
+			// before exit stop all threads
+			stopThreads(threadArr);
+			break;
+		}
+		if(passwdFound){
+			// stop all threads
+			stopThreads(threadArr);
+			// show found password
+			std::cout << "The deciphered password is: " << passwdPairs.back()[0]
+			 << ". Associated email: " << passwdPairs.back()[2] << std::endl;
+			// erase password from the list 
+			hash.pop_back();
+			emails.pop_back();
+		}
+		if(!passwdFound) passwdFile.open(userInput, std::ios::out);
+		if(!passwdFile){
 			std::cout << "No file of such name in current directory."
 			 << std::endl;
 		}
-		else{
-
+		else if(!passwdFound){
+			// stop threads execution
+			stopThreads(threadArr);
+			// read new hashed passwords to memory
+			std::string lineHash, lineEmail, trash;
+			if(passwdFile.is_open()) {
+				while(passwdFile){
+					passwdFile >> trash >> lineHash >> lineEmail;
+					std::getline(passwdFile, trash);
+      				hash.push_back(lineHash);
+					emails.push_back(lineEmail);
+				}
+				std::cout << "Passwords and emails successfuly read to memory." << std::endl;
+				passwdFile.close();
+			}
+			// start thread to find passwords
+			threadArr[0] = std::thread(producer1word, hash.back(), producerNR::one);
 		}
-
 	}
-	
-	std::string line;
-    if(file.is_open()) {
-		while(file) {
-			getline(file, line);
-			dictionary.push_back(line);
-		}
-	}
-
-    char md5[33]; // 32 characters + null terminator
-    for(int i = 0; i < dictionary.size(); i++){
-      const char *word = dictionary[i].c_str();
-      bytes2md5(word, strlen(word), md5);
-      hash.push_back(md5);
-    }
-
-    
-
     return 0;
 }
-
-/*
-// Check if dictionary file is given
-    if (argc < 2)
-    {
-        cout << "Argument 1 not specified. Filename required!\n";
-        exit(-1);
-    }
-
-*/
