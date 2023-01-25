b7c29c82725b1572245aba34318c2c6d11ae8f2a
Olaf Bieganski <olaf.bieganski2001@gmail.com>
2023-01-25 17:10:25 +0100

    Wersja 2 - nie dziala (pierwsze podejscie)

--- lista_10/passwd_decryption.cpp
+++ lista_10/passwd_decryption.cpp
@@ -7,18 +7,21 @@
 #include <array>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 
-#define THREAD_NR 3
+#define THREAD_NR 3 // number of searching threads
 
 std::vector<std::string> dictionary, hash, emails;
 std::vector<std::array<std::string, 3>> passwdPairs; // first is pswd second hash, third email
 bool passwdFound = false; // flags  for threads
-bool continueFlag = false;
+bool continueFlag = true;
 bool isInput = false;
+bool threadsFinished = false;
 std::array<std::thread, THREAD_NR> threadArr;
 std::array<bool, THREAD_NR> thread_state; // inform if threads finished with no result
 std::string userInput;
-std::mutex passwdFound_mtx, passwdPairs_mtx, userInput_mtx, dict_mtx; 
+std::mutex passwdFound_mtx, passwdPairs_mtx, userInput_mtx, dict_mtx, hashEmail_mtx, cont_mtx, threads_finished_mtx;
+std::condition_variable newPasswd_CV, newInput_CV, threadsFinished_CV;
 
 // function to peform conversion from char array to hash code array
 void bytes2md5(const char *data, int len, char *md5buf) {
@@ -76,35 +79,56 @@ const std::string readUserInput(){
 void getInput(){
 	std::string tempInput;
 	std::cout << "Provide file with passwords or \"q\" for exit. \n";
-	while(readUserInput() != "q"){
+	while(userInput != "q"){
 		// get whole line from input
 		std::getline(std::cin, tempInput);
-		userInput_mtx.lock();
+		std::unique_lock<std::mutex> lock(userInput_mtx);
 		userInput = tempInput;
 		isInput = true;
-		userInput_mtx.unlock();
+		newInput_CV.notify_one();
+		lock.unlock(); // may cause an error?
 	}
 }
 
-bool checkPswd(const std::string &currWord, const std::string &_crrhash){
+void checkPswd(const std::string &currWord){
+	if(hash.empty()) return;
 	char md5[33]; // 32 characters + null terminator
 	const char *word = currWord.c_str();
 	bytes2md5(word, strlen(word), md5);
-	if(md5 == _crrhash){
-		std::array<std::string, 3> newPswd = {currWord, _crrhash, emails.back()};
-		// protect with mutex
-		passwdPairs_mtx.lock();
-		passwdPairs.push_back(newPswd);
-		passwdPairs_mtx.unlock();
-		mtx_protwrt(passwdFound_mtx, passwdFound, true);
-		return true;
+	for(auto it = hash.begin(); it != hash.end(); ){
+		if(*it == md5){
+			uint idx = std::distance(hash.begin(), it);
+			std::array<std::string, 3> newPswd = {currWord, hash[idx], emails[idx]};
+			//delete hash and email from list
+			hashEmail_mtx.lock();
+			it = hash.erase(it);
+			emails.erase(emails.begin() + idx);
+			hashEmail_mtx.unlock();
+			// inform consumer that passwd found via condVar
+			std::unique_lock<std::mutex> lock(passwdFound_mtx);
+			passwdPairs.push_back(newPswd);
+			passwdFound = true;
+			newPasswd_CV.notify_one();
+			if(hash.empty()){
+				// stop threads
+				mtx_protwrt(cont_mtx, continueFlag,false);
+				// notify inform thread
+				threadsFinished_CV.notify_one();
+				std::cout << "End of the password list. Provide new file." << std::endl;
+				return;
+			}
+			lock.unlock(); // may cause an error?
+		}
 	}
-	return false;
 }
 
 enum class producerNR {zero, one, two};
+enum class digitPlace {noDigit, prefixDigit, postfixDigit, middleDigit, prePostDigit};
+std::array<std::string,16> symbols = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+ "!", "@", "#", "$", "%", "&"};
 
-void producer1word(const std::string &_crrhash, producerNR _producer){
+//
+/*void producer1word(const std::string &_crrhash, producerNR _producer){
 
 	for(const std::string &x : dictionary){
 
@@ -180,24 +204,105 @@ void producer1word(const std::string &_crrhash, producerNR _producer){
 			state = true;
 			break;
 		}
-}
+}*/
 
-void producer2word(const std::string &_crrhash){
+// possible check for 2 word passwd with all letter size cominations
+// and pre, post and middle additional numbers up to 2 symbols gigit and special signs combinations
+void producer2word(producerNR _producer, digitPlace _digitPlace){
+	for(const std::string &x : dictionary){
 
+		std::string word1 = mtx_protrd(dict_mtx, x);
+		// first word letter size based on thread argument
+		switch(_producer){
+			case producerNR::one:
+				word1[0] = toupper(word1[0]);
+				break;
+			case producerNR::two:
+				for(auto &letter : word1)
+					letter = toupper(letter);
+				break;
+		}
+
+		for(const std::string &y : dictionary){
+			// all possible letter size combinations for second word
+			if(!mtx_protrd(cont_mtx, continueFlag)) return;
+			std::string word2 = mtx_protrd(dict_mtx, y);
+			auto word2FUP = word2;
+			word2FUP[0] = toupper(word2[0]);
+			auto word2AUP = word2;
+			for(auto &letter : word2AUP)
+				letter = toupper(letter);
+			// adding digits
+			switch(_digitPlace){
+				case digitPlace::noDigit: break;
+				case digitPlace::prefixDigit:
+					for(auto &sym : symbols){
+						auto prefix = sym;
+						for(auto &sym2 : symbols){
+							if(!mtx_protrd(cont_mtx, continueFlag)) return;
+							prefix = prefix + sym2;
+							checkPswd(prefix + word1 + word2);
+							checkPswd(prefix + word1 + word2FUP);
+							checkPswd(prefix + word1 + word2AUP);
+						}
+					}
+				break;
+				case digitPlace::postfixDigit:
+					for(auto &sym : symbols){
+						auto postfix = sym;
+						for(auto &sym2 : symbols){
+							if(!mtx_protrd(cont_mtx, continueFlag)) return;
+							postfix = postfix + sym2;
+							checkPswd(word1 + word2 + postfix);
+							checkPswd(word1 + word2FUP + postfix);
+							checkPswd(word1 + word2AUP + postfix);
+						}
+					}
+				break;
+				case digitPlace::middleDigit:
+					for(auto &sym : symbols){
+						auto middle = sym;
+						for(auto &sym2 : symbols){
+							if(!mtx_protrd(cont_mtx, continueFlag)) return;
+							middle = middle + sym2;
+							checkPswd(word1 + middle + word2);
+							checkPswd(word1 + middle + word2FUP);
+							checkPswd(word1 + middle + word2AUP);
+						}
+					}
+				break;
+				case digitPlace::prePostDigit:
+					for(uint i = 0; i < 100; i++){
+						auto prefix = std::to_string(i);
+						for(uint i = 0; i < 100; i++){
+							if(!mtx_protrd(cont_mtx, continueFlag)) return;
+							auto postfix = std::to_string(i);
+							checkPswd(prefix + word1 + word2  + postfix);
+							checkPswd(prefix + word1 + word2FUP + postfix);
+							checkPswd(prefix + word1 + word2AUP + postfix);
+						}
+					}
+				break;
+			}
+			// check all three options
+			if(_digitPlace != digitPlace::noDigit) break;
+			checkPswd(word1 + word2);
+			checkPswd(word1 + word2FUP);
+			checkPswd(word1 + word2AUP);
+		}
+	}
 }
 
 template<std::size_t SIZE>
 void stopThreads(std::array<std::thread, SIZE> &thArr){
 	// stop all threads
 	continueFlag = false;
+	//wait a litlle bit
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	// joining threads to terminate them
 	for(int i = 0; i < SIZE; i++)
 		if(threadArr[i].joinable())
 			threadArr[i].join();
-	// setting state as unfinisehd
-	for(auto &state: thread_state)
-		state = false;
-	continueFlag = true;
 }
 
 bool threadNoResult(){
@@ -207,23 +312,59 @@ bool threadNoResult(){
 	return !passwdFound;
 }
 
-void threadStartnewSearch(bool &_end_reached){
-	if(!hash.empty()) hash.pop_back();
-	if(!emails.empty()) emails.pop_back();
-	// prepare to start new search
-	mtx_protwrt(passwdFound_mtx, passwdFound, false);
-	//setting state as unfinished
+// before use of this function you have to stop all running threads
+// and run utility threads
+void threadStartnewSearch(){
+	for(auto &state : thread_state) state = false;
+	threadArr[0] = std::thread(producer2word, producerNR::zero, digitPlace::prePostDigit);
+	//threadArr[1] = std::thread(producer2word, producerNR::one, digitPlace::prefixDigit);
+	//threadArr[2] = std::thread(producer2word, producerNR::two, digitPlace::postfixDigit);
+}
+
+// sets thread's state as finished after work is done
+// and notify suitable thread if all have finished
+void threadSetState(){
+	// set state as work finished
 	for(auto &state : thread_state)
-		state = false;
-	// if hash list empty finish searching
-	if(hash.empty()){
-		_end_reached = true;
-		return;
+		if(state == false){
+			state = true;
+			break;
+		}
+	// if still false values do not notify
+	for(auto &state : thread_state)
+		if(state == false) return;
+	// if all threads finished notify about it
+	std::unique_lock<std::mutex> lock(threads_finished_mtx);
+	threadsFinished = true;
+	threadsFinished_CV.notify_one();
+}
+
+void consumer(){
+	// setting initial state
+	continueFlag = true;
+	passwdFound = false;
+	// while threads run check for new passwd
+	while(continueFlag){
+		std::unique_lock<std::mutex> lock(passwdFound_mtx);
+		if(!passwdFound){
+			newPasswd_CV.wait(lock);
+		}
+		std::cout << "The deciphered password is: " << passwdPairs.back()[0]
+			<< ". Associated email: " << passwdPairs.back()[2] << std::endl;
+		passwdFound = false;
 	}
-	// all threads' initialization
-	threadArr[0] = std::thread(producer1word, hash.back(), producerNR::zero);
-	threadArr[1] = std::thread(producer1word, hash.back(), producerNR::one);
-	threadArr[2] = std::thread(producer1word, hash.back(), producerNR::two);
+}
+
+// has to be called with working threads each time new passwds provided
+void informThreadsFinish(){
+	threadsFinished = false;
+	std::unique_lock<std::mutex> lock(threads_finished_mtx);
+	if(!threadsFinished){
+		threadsFinished_CV.wait(lock);
+	}
+	if(continueFlag) return; // if threads finished cause of contFlag do not print
+	std::cout << "All threads finished its work. Not all passwords have been found. Provide new file" << 
+		" or \"q\" for exit." << std::endl;
 }
 
 int main(int argc, char* argv[]){
@@ -257,58 +398,31 @@ int main(int argc, char* argv[]){
 	}
 	
 	// start user input handling thread
-	std::thread inputThread(getInput);
-
-	// variable for end of passwd list
-	bool end_reached = false;
+	std::thread inputThread(getInput), consumerThread, informFinish;
 
 	while(true){
-		// input is handled in separate thread
-		while(!(isInput || passwdFound)){
-			if(threadNoResult() && !hash.empty()){
-				if(!emails.empty()){
-					std::cout << "No password was found for: "
-				 	 << emails.back() << std::endl;
-				}
-				stopThreads(threadArr);
-				threadStartnewSearch(end_reached);
-			}
-			if((hash.empty() && threadNoResult()) || end_reached){
-				std::cout << "Passwords list's end reached."
-				 << " Enter new file name or \"q\" for exit." << std::endl;
-				while(!isInput);
-			}
+		std::unique_lock<std::mutex> lock(userInput_mtx);
+		if(!(isInput)){
+			newInput_CV.wait(lock);
 		}
-		// stop if q is input
-		if(readUserInput() == "q"){
+		if(userInput == "q"){
 			// before exit stop all threads
-			inputThread.join();
 			stopThreads(threadArr);
+			if(inputThread.joinable()) inputThread.join();
+			if(consumerThread.joinable()) consumerThread.join();
+			if(informFinish.joinable()) informFinish.join();
 			break;
 		}
-		if(passwdFound){
-			// stop all threads
-			stopThreads(threadArr);
-			// show found password
-			std::cout << "The deciphered password is: " << passwdPairs.back()[0]
-			 << ". Associated email: " << passwdPairs.back()[2] << std::endl;
-			// start new search
-			threadStartnewSearch(end_reached);
-		}
-		if(isInput) passwdFile.open(readUserInput(), std::ios::out);
-		if(!passwdFile && isInput){
+		passwdFile.open(userInput, std::ios::out);
+		if(!passwdFile){
 			std::cout << "No file of such name in current directory. Provide correct filename."
 			 << std::endl;
-			userInput_mtx.lock();
-			isInput = false;
-			userInput_mtx.unlock();
 		}
-		if(isInput){
-			userInput_mtx.lock();
-			isInput = false;
-			userInput_mtx.unlock();
+		if(passwdFile){
 			// stop threads' execution
 			stopThreads(threadArr);
+			if(consumerThread.joinable()) consumerThread.join();
+			if(informFinish.joinable()) informFinish.join();
 			// clear data structures
 			hash.clear();
 			emails.clear();
@@ -316,18 +430,21 @@ int main(int argc, char* argv[]){
 			// read new hashed passwords to memory
 			std::string lineHash, lineEmail, trash;
 			if(passwdFile.is_open()) {
- 				do{
+				do{
 					passwdFile >> trash >> lineHash >> lineEmail;
 					std::getline(passwdFile, trash);
-      				hash.push_back(lineHash);
+					hash.push_back(lineHash);
 					emails.push_back(lineEmail);
 				}while(passwdFile);
 				std::cout << "Passwords and emails successfuly read to memory. Starting new search." << std::endl;
 				passwdFile.close();
-				end_reached = false; // end not reached
 			}
+			isInput = false;
+			lock.unlock(); // here?
 			// start threads to find passwords
-			threadStartnewSearch(end_reached);
+			consumerThread = std::thread(consumer);
+			informFinish = std::thread(informThreadsFinish);
+			threadStartnewSearch();
 		}
 	}
     return 0;
